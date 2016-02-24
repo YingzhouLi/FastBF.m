55276042921f76959cc151de7efca28b8148939e
YingzhouLi <lyzh588@gmail.com>
2016-02-24 13:22:48 -0800

    Updated the relation for NG and tol in batch fastbf 1D

--- test/batch_fastbf_1D.m
+++ test/batch_fastbf_1D.m
@@ -4,13 +4,17 @@ if(~exist(log_path, 'dir'))
     mkdir(log_path);
 end
 
+NGlist = [4 6 8];
+tollist = [1e-2 1e-4 1e-6];
+
 func_list = {'fun0'};
 for func_i = 1:length(func_list)
     func_name = func_list{func_i};
     for N = 2.^(8:2:18)
         fid = fopen([log_path 'Factor_' func_name '_' num2str(N) '.log'],'a+');
-        for NG = 4:2:8
-            tol = 1e-4;
+        for iter = 1:length(NGlist)
+            NG = NGlist(iter);
+            tol = tollist(iter);
             run_fastbf_1D(N, func_name, NG, tol, fid);
             fprintf('Func %s, N %4d, NG %2d finished.\n',func_list{func_i},N,NG);
         end
