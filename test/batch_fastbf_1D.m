94e5c0da8d443b62553bb62c51749ffcb045ebed
YingzhouLi <ryanli@stanford.edu>
2016-03-05 15:14:59 -0800

    Updated tols

--- test/batch_fastbf_1D.m
+++ test/batch_fastbf_1D.m
@@ -5,12 +5,12 @@ if(~exist(log_path, 'dir'))
 end
 
 NGlist = [4 6 8];
-tollist = [1e-2 1e-2 1e-4];
+tollist = [1e-2 1e-4 1e-6];
 
 func_list = {'fun0'};
 for func_i = 1:length(func_list)
     func_name = func_list{func_i};
-    for N = 2.^(20:2:20)
+    for N = 2.^(8:2:20)
         fid = fopen([log_path 'Factor_' func_name '_1D_' num2str(N) '.log'],'a+');
         for iter = 1:length(NGlist)
             NG = NGlist(iter);
